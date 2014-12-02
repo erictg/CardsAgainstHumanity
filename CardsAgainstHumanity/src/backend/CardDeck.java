8748f8d39b6f79fcaf56b38d414eee9faec4fbba
Eric Solender <ericsolender@gmail.com>
2014-12-01 19:26:23 -0500

    edits

---
+++ CardsAgainstHumanity/src/backend/CardDeck.java
@@ -0,0 +1,6 @@
+package backend;
+import java.util.*;
+public class CardDeck {
+
+	ArrayList<Card> deck = new Arr
+}
--- CardsAgainstHumanity/src/backend/Deck.java
+++ CardsAgainstHumanity/src/backend/Deck.java
@@ -1,32 +1,71 @@
 package backend;
+import java.io.File;
+import java.util.ArrayList;
 
-import java.util.*;
-import java.io.*;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
 public class Deck {
 
-	ArrayList<Card> cards = new ArrayList<Card>();
+	private int typeID;
+	private String deckName;
+	private ArrayList<String> cards = new ArrayList<String>();
+	private File file;
+	public static final int BLACK_CARD = 0;
+	public static final int WHITE_CARD = 0;
 	
-	public Deck(String deckName){
-		try {
-			readInDeck(deckName);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public Deck(){
+		typeID = 0;
+		deckName = "";
+		setFile(null);
+		//addCard("default");
+	}
+	public Deck(int typeID, String deckName, File f){
+		this.typeID = typeID;
+		this.deckName = deckName;
+		setFile(f);
 	}
 	
-	public void readInDeck(String deckName) throws IOException, FileNotFoundException{
-		BufferedReader rd = new BufferedReader(new FileReader("src//decks//" + deckName + ".cah"));
-		int id = 0;
-		while(rd.readLine() != null){
-			cards.add(new Card(rd.readLine(), id));
-			id++;
-		}
-		rd.close();
-		
+	public Deck(int typeID, String deckName, File f, ArrayList<String> cards){
+		this.typeID = typeID;
+		this.deckName = deckName;
+		this.cards = cards;
+		setFile(f);
+	}
+
+	public void addCard(String s){
+		cards.add(s);
 	}
 	
+	public void removeCard(String s){
+		cards.remove(s);
+	}
+	public int getTypeID() {
+		return typeID;
+	}
+
+	public void setTypeID(int typeID) {
+		this.typeID = typeID;
+	}
+
+	public String getDeckName() {
+		return deckName;
+	}
+
+	public void setDeckName(String deckName) {
+		this.deckName = deckName;
+	}
+
+	public ArrayList<String> getCards() {
+		return cards;
+	}
+
+	public void setCards(ArrayList<String> cards) {
+		this.cards = cards;
+	}
+	public File getFile() {
+		return file;
+	}
+	public void setFile(File file) {
+		this.file = file;
+	}
 }
